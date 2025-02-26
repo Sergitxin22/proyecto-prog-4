cc7ffd6f35ed07a5bf5afc3ad2f01801c1388c09
Markel Mencía <markel.mencia@opendeusto.es>
2025-02-26 16:44:01 +0100

    Add tests for shell commands

---
+++ tests/test_shell.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "../shell.c"
+
+void testPrintArgs(char *string, int *arg_count);
+
+int main(int argc, char const *argv[])
+{
+    
+    int count;
+    // Regular input test
+    testPrintArgs("Hello world! \"This string\" has 6 arguments", &count);
+    printf("\n");
+    // Correct space trimming test
+    testPrintArgs("  Hello world!   This string     has 7 arguments  ", &count);
+    printf("\n");
+    // Quotes at the beginning and end of string test 
+    testPrintArgs("\"Hello world! This string has 1 argument\"", &count);
+    printf("\n");
+    // Wrong use of quotes test
+    testPrintArgs("Hello world! this string is wrong\" ", &count);
+    printf("\n");
+    // Wrong use of quotes test
+    testPrintArgs("Hello world! \"\"this string has 7 arguments", &count);
+    printf("\n");
+    // Shell prompt test
+    printf("%s", prompt());
+    return 0;
+}
+
+void testPrintArgs(char *string, int *arg_count) {
+    char **args = splitArgs(string, arg_count);
+
+    for (int i = 0; i < *arg_count; i++) {
+        printf("Argument %d: %s\n", i, args[i]);
+    }
+    
+    printf("Total arguments printed: %d\n", *arg_count);
+    free(args);
+}
+
