1a9b1115f6e343c01a15a8d92b763cd9de79c993
Sergitxin22 <sergio.morales@opendeusto.es>
2025-03-23 22:38:48 +0100

    Fix argument issue in exec.c

--- exec.c
+++ exec.c
@@ -3,17 +3,19 @@
 #include <string.h>
 #include "headers/commands.h"
 
-//Comando de prueba
-int pp_cmd(char** args) {
-    printf("Comando PP ejecutado correctamente.");
+// Comando de prueba
+int pp_cmd(int argc, char **args)
+{
+    printf("Comando PP ejecutado correctamente.\n");
 }
-//Array de Comandos (Structs)
-const Command commands[] = {
-    {"PP",&pp_cmd},
-    {"mkdir",&mkdir}
-};
 
-//Tamaño del Array de comandos
+// Array de Comandos (Structs)
+const Command commands[] = {
+    {"PP", &pp_cmd},
+    {"mkdir", &mkdir},
+    {"ver", &ver_cmd}};
+
+// Tamaño del Array de comandos
 const int lenCommand = sizeof(commands) / sizeof(Command);
 
 /**
@@ -22,24 +24,29 @@ const int lenCommand = sizeof(commands) / sizeof(Command);
  * @return Un entero que devuelve el codigo de ejecuccion del comando para saber si ha sido correcto
  * o ha habido algun fallo.
  */
-int exec(char **args){
+int exec(int argc, char **args)
+{
 
-    //Si el primer argumento (nombre del programa) esta vacio , lanzamos error
-    if (strcmp(args[0],"") == 0) {
-        perror("Error : No se ha recibido ningun comando! \n" );
+    // Si el primer argumento (nombre del programa) esta vacio , lanzamos error
+    if (strcmp(args[0], "") == 0)
+    {
+        perror("Error : No se ha recibido ningun comando! \n");
         return -1;
     }
 
-    //Comprueba si el primer argumento (Empieza en 0) el cual es el nombre del programa
-    //Esta en nuestro struct, si esta se ejecuta
-    for (int i = 0; i < lenCommand; i++) {
-        if (strcmp(commands[i].name,args[0]) == 0) {
-            //args + 1 devuelve el puntero a la siguiente posicion de memoria
-            // es decir, pasamos el array pero sin el primer elemento
-            commands[i].commandPtr(args + 1);
+    // Comprueba si el primer argumento (Empieza en 0) el cual es el nombre del programa
+    // Esta en nuestro struct, si esta se ejecuta
+    for (int i = 0; i < lenCommand; i++)
+    {
+        if (strcmp(commands[i].name, args[0]) == 0)
+        {
+            // args + 1 devuelve el puntero a la siguiente posicion de memoria
+            //  es decir, pasamos el array pero sin el primer elemento
+            // TODO: Eliminar correctamente el primer argumento recibido
+            commands[i].commandPtr(argc, args);
             return 0;
         }
     }
-    fprintf(stderr,"%s no es un comando valido! \n",args[0]);
+    fprintf(stderr, "%s no es un comando valido! \n", args[0]);
     return -2;
 }
