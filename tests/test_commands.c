c317510a8f7c0d5cb22eb5524700ab25ee348354
Markel Mencia <markel.mnc@gmail.com>
2025-03-31 20:01:02 +0200

    Create writeline command test

--- tests/test_commands.c
+++ tests/test_commands.c
@@ -49,9 +49,37 @@ void test_cd() {
     free(args);
 }
 
+void test_writeline() {
+    // Creará un fichero .txt en el directorio
+    // de ejecución del test
+    char **args = malloc(sizeof(char *) * 3);
+    char *arg = "writeline";
+    char *arg2 = "testfile.txt";
+    char *arg3 = "hello world";
+    args[0] = arg;
+    args[1] = arg2;
+    args[2] = arg3;
+    writeline_cmd(3, args);
+
+    char **args2 = malloc(sizeof(char *) * 4);
+    char *option = "-a";
+    arg = "writeline";
+    arg2 = "testfile.txt";
+    arg3 = "programmed to work and not to feel";
+    args2[0] = arg;
+    args2[1] = option;
+    args2[2] = arg2;
+    args2[3] = arg3;
+    writeline_cmd(4, args2);
+
+    free(args);
+    free(args2);
+}
+
 int main(int argc, char const *argv[]) {
     test_ver();
     test_printdir();
     test_cd();
+    test_writeline();
     return 0;
 }
