ac8f4f75a3104fb79c8092b140cafc81e931a9f4
Markel Mencia <markel.mnc@gmail.com>
2025-03-31 17:56:51 +0200

    Create printdir test

--- tests/test_commands.c
+++ tests/test_commands.c
@@ -1,18 +1,15 @@
 #include "../headers/commands.h"
 #include <stdlib.h>
 
-void test_ver()
-{
+void test_ver() {
     // Versión larga
-    char *arg = malloc(sizeof(char) * 4);
-    arg = "ver";
+    char *arg = "ver";
     char **args = malloc(sizeof(char *) * 1);
     args[0] = arg;
     ver_cmd(1, args);
 
     // Versión corta (-n)
-    char *arg2 = malloc(sizeof(char) * 3);
-    arg2 = "-n";
+    char *arg2 = "-n";
     char **args2 = malloc(sizeof(char *) * 1);
     args2[0] = arg;
     args2[1] = arg2;
@@ -22,8 +19,25 @@ void test_ver()
     free(args2);
 }
 
-int main(int argc, char const *argv[])
-{
+void test_printdir() {
+    // Versión larga
+    char **args = malloc(sizeof(char *));
+    printdir_cmd(1, args);
+
+    // Versión corta
+    char *arg = "printdir";
+    char *arg2 = "-n";
+    char **args2 = malloc(sizeof(char *));
+    args2[0] = arg;
+    args2[1] = arg2;
+    printdir_cmd(2, args2);
+
+    free(args);
+    free(args2);
+}
+
+int main(int argc, char const *argv[]) {
     test_ver();
+    test_printdir();
     return 0;
 }
