4e543532849e9aaf24fa818449414562703490bc
Markel Mencia <markel.mnc@gmail.com>
2025-04-05 12:15:32 +0200

    Fix header definition

--- headers/db.h
+++ headers/db.h
@@ -1,11 +1,10 @@
-
 #ifndef DB_H
 #define DB_H
 #include "../headers/shell.h"
 #include "../lib/sqlite3/sqlite3.h"
 #include <stdio.h>
 #include <stdlib.h>
-#endif //DB_H
+
 
 sqlite3 *openDatabase();
 User* getAllUsers(int  *count);
@@ -15,5 +14,6 @@ void deleteUserDB(char *username);
 int is_user_admin(char *username);
 int verify_password(char *username, char *password);
 
+#endif //DB_H
 
 
