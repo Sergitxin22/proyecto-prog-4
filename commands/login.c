79e0e84921b2cb45d372b6b19df1fd3b389d3f9e
Markel Mencia <markel.mnc@gmail.com>
2025-04-05 11:44:10 +0200

    Add database functionality to login command

--- commands/login.c
+++ commands/login.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "../headers/shell.h"
 #include "../headers/commands.h"
+#include "../headers/db.h"
+#include "../lib/sqlite3/sqlite3.h"
 
 
 /**
@@ -11,16 +14,31 @@
  * @return int 0 si el login es exitoso, 1 si falla
  */
 int login_cmd(int argc, char** args) {
-    char username[20];
-    char password[20];
-    char command[50];
+    char username[30];
+    char password[30];
 
     // Pedir el nombre de usuario
     printf("Username: ");
     scanf("%s", username);
+    while(getchar() != '\n');
 
+    if (!userExists(username)) {
+        fprintf(stderr, "login: user does not exist\n");
+        return -3;
+    }
     // Pedir la contraseña
-    printf("Password: ");
+    printf("%s's password: ", username);
     scanf("%s", password);
+    while(getchar() != '\n');
+
+    if (!verify_password(username, password)) {
+        fprintf(stderr, "login: incorrect password\n");
+        return -4;
+    }
+
+    strcpy(CURRENT_USER.username, username);
+    CURRENT_USER.user_type = is_user_admin(username);
+    printf("Login correct. Welcome, %s\n", username);
+    return 0;
 
 }
