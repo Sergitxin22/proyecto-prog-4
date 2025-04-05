8f39e35f2e6dc9f472c03baaf255f10ae91b4f82
Markel Mencia <markel.mnc@gmail.com>
2025-04-05 11:44:33 +0200

    Create User struct

--- headers/shell.h
+++ headers/shell.h
@@ -4,27 +4,31 @@
 #define MAX_ARG_LEN 64
 #define MAX_PROMPT_LEN 128
 #include <stddef.h>
-//variable global
-extern int current_user_type;
+
+typedef struct User {
+    char username[30];
+    int user_type;
+} User;
+
+// Variable global
+extern User CURRENT_USER;
 
 int isAdmin(void);
-
 
 int prompt(char** line);
 char** splitArgs(char *input_line, int *arg_count);
 char** getcommands(size_t* outSize);
 
 int exec(int argc, char **args);
+
 /**
 * Estructura COMMAND que contiene el nombre del Comando y un puntero a la funcion
 que ejecuta el comando.
 Los argumentos del comando (args) son las opciones que le pasa el usuario, NULL si ninguna opcion.
 */
-typedef struct Command
-{
+typedef struct Command {
     char *name;
     int (*commandPtr)(int argc, char **args);
-
 } Command;
 
 #endif // SHELL_H
