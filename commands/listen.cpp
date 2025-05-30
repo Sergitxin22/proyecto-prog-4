2936071ffd2764c0ae6e0a4363397942c1d7b336
Julen <julen.o.ramirez@opendeusto.es>
2025-05-30 09:24:14 +0200

    Update listen.cpp
    Modify exec call to include mode parameter set to 1 (remote)

--- commands/listen.cpp
+++ commands/listen.cpp
@@ -21,61 +21,46 @@
 Status listen_cmd(int argc, const char **args)
 {
     signal(SIGPIPE, SIG_IGN);
-    
-    if (!CURRENT_USER->isAdmin()) {
+
+    if (!CURRENT_USER->isAdmin())
+    {
         return Status(-1, "listen is not a valid command\n");
     }
 
-    if (argc < 2) {
+    if (argc < 2)
+    {
         return Status(-1, "listen: incorrent number of arguments\n");
     }
 
-    // creating socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    // specifying the address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(atoi(args[1]));
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
     listen(serverSocket, 5);
     printf("Listening for incoming connections...\n");
     int clientSocket = accept(serverSocket, nullptr, nullptr);
     printf("User connected\n");
-    while(1) {
-
-        // recieving data
+    while (1)
+    {
         char buffer[1024] = {0};
-        int bytes_received = recv(clientSocket, buffer,1024, 0);
-        if (bytes_received <= 0) {
+        int bytes_received = recv(clientSocket, buffer, 1024, 0);
+        if (bytes_received <= 0)
+        {
             printf("Host disconnected\n");
             break;
         }
 
         NetCommandRequest req = NetCommandRequest("");
         req.deserialize(buffer);
-    
-        Status *status = NULL;
 
         int arg_count = 0;
         const char **args = splitArgs(req.getCommand(), &arg_count);
         if (arg_count != -1)
         {
-            status = exec(arg_count, args);
-            // Se imprime el output en el flujo correcto,
-            // (en función del código de estado)
-            if (!status->isOutputEmpty())
-            {
-                if (status->getStatus() == 0)
-                {
-                    fprintf(stdout, "%s", status->getOutput());
-                }
-                else
-                {
-                    fprintf(stderr, "%s", status->getOutput());
-                }
-            }
+            exec(arg_count, args, 1); // Modo remoto
         }
 
         for (int i = 0; i < arg_count; i++)
@@ -83,14 +68,8 @@ Status listen_cmd(int argc, const char **args)
             free((void *)args[i]);
         }
         free(args);
-        
-        char response[1024] = {0};
-        status->serialize(response);
-        send(clientSocket,response,1024, 0);
-        delete(status);
-      
     }
-    // closing the socket.
+
     close(serverSocket);
     close(clientSocket);
     return Status(0);
